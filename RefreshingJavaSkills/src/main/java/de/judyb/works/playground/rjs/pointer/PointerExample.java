16f3baef1d9e9bd0fd2e99ef5dbfec3af3dc94d3
judyb-works <judyb1982@googlemail.com>
2019-12-19 20:37:44 +0100

    created simple idea-project with simple maven-module

---
+++ RefreshingJavaSkills/src/main/java/de/judyb/works/playground/rjs/pointer/PointerExample.java
@@ -0,0 +1,17 @@
+package de.judyb.works.playground.rjs.pointer;
+
+public class PointerExample {
+    private int value;
+
+    public PointerExample(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
